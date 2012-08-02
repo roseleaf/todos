4dd9516dc1a19a37bf5c331d5781aff7b6d23427
roseleaf <pteroline@gmail.com>
2012-08-02 13:43:45 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'f2da310cbdfa4eba2366a09afbc6b86566caa9c2eb641d704c3e6ca6892f7a335b0d47aa9fd28e3862667643d6db9420485607ca5c8478bd47f90351d4852bdf'
